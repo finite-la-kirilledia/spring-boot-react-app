ccb3b99a6f8dc77b21997db8e214340730a18f1c
finite-la-kirilledia <kirill040994@gmail.com>
2022-09-06 23:35:51 +0200

    backend: extract data from db using JDBC

--- src/main/java/com/finite/repo/StudentRepo.java
+++ src/main/java/com/finite/repo/StudentRepo.java
@@ -1,6 +1,9 @@
 package com.finite.repo;
 
 import com.finite.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,18 +12,30 @@ import java.util.UUID;
 @Repository
 public class StudentRepo {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public StudentRepo(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                Student.builder()
-                        .id(UUID.randomUUID())
-                        .firstName("John")
-                        .age(28)
-                        .build(),
-                Student.builder()
-                        .id(UUID.randomUUID())
-                        .firstName("Jack")
-                        .age(22)
-                        .build()
+        return jdbcTemplate.query(
+                "SELECT id, first_name, age FROM student",
+                mapStudentFromDb()
         );
+    }
+
+    private RowMapper<Student> mapStudentFromDb() {
+        return (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String firstName = resultSet.getString("first_name");
+            int age = resultSet.getInt("age");
+            return Student.builder()
+                    .id(id)
+                    .firstName(firstName)
+                    .age(age)
+                    .build();
+        };
     }
 }
