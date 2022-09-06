9e035caf792567eeb4402dfd0e38b90f86531eeb
finite-la-kirilledia <kirill040994@gmail.com>
2022-09-06 23:05:08 +0200

    backend: configure database

---
+++ src/main/java/com/finite/datasource/Datasource.java
@@ -0,0 +1,19 @@
+package com.finite.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Datasource {
+
+    @Bean
+    @ConfigurationProperties("datasource")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
