02587cfcfb5f524cab75ee01dc285ee02869358f
finite-la-kirilledia <kirill040994@gmail.com>
2022-09-07 21:39:59 +0200

    frontend: refresh students after submit and refactor

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -97,11 +97,16 @@ class App extends Component {
             onOk={this.closeAddStudentModal}
             onCancel={this.closeAddStudentModal}
             width={1000}>
-            <AddStudentForm/>
+            <AddStudentForm
+              onSuccess={() => {
+                this.closeAddStudentModal();
+                this.fetchStudents();
+              }}
+            />
           </Modal>
-          <Footer 
-          numberOfStudents={students.length}
-          handleAddStudentClickEvent={this.openAddStudentModal}/>
+          <Footer
+            numberOfStudents={students.length}
+            handleAddStudentClickEvent={this.openAddStudentModal} />
         </Container>
 
       );
--- src/main/frontend/src/forms/AddStudentForm.js
+++ src/main/frontend/src/forms/AddStudentForm.js
@@ -1,4 +1,4 @@
-import React, { Component, isValidElement } from "react";
+import React from "react";
 import { Formik } from "formik";
 import { Input, Button, Tag } from 'antd';
 import { addNewStudent } from "../client";
@@ -6,74 +6,71 @@ import { addNewStudent } from "../client";
 const inputBottomMargin = { marginBottom: '10px' };
 const tagStyle = { backgroundColor: 'red', color: 'white', ...inputBottomMargin };
 
-class AddStudentForm extends Component {
-    render() {
-        return (
-            <Formik
-                initialValues={{ firstName: '', age: '' }}
-                validate={values => {
-                    const errors = {};
+const AddStudentForm = (props) => (
+    <Formik
+        initialValues={{ firstName: '', age: '' }}
+        validate={values => {
+            const errors = {};
 
-                    if (!values.firstName) {
-                        errors.firstName = 'Please enter first name';
-                    }
+            if (!values.firstName) {
+                errors.firstName = 'Please enter first name';
+            }
 
-                    if (!values.age) {
-                        errors.age = 'Please enter age';
-                    } else if (isNaN(values.age)) {
-                        errors.age = 'Age should be a number';
-                    }
+            if (!values.age) {
+                errors.age = 'Please enter age';
+            } else if (isNaN(values.age)) {
+                errors.age = 'Age should be a number';
+            }
 
-                    return errors;
-                }}
-                onSubmit={(student, { setSubmitting }) => {
-                    addNewStudent(student).then(() => {
-                        setSubmitting(false);
-                    })
-                }}
-            >
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting,
-                    submitForm,
-                    isValid
-                }) => (
-                    <form onSubmit={handleSubmit}>
-                        <Input
-                            style={inputBottomMargin}
-                            name="firstName"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.firstName}
-                            placeholder='First name'
-                        />
-                        {errors.firstName && touched.firstName && <Tag style={tagStyle}>{errors.firstName}</Tag>}
-                        <Input
-                            style={inputBottomMargin}
-                            name="age"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.age}
-                            placeholder='Age'
-                        />
-                        {errors.age && touched.age && <Tag style={tagStyle}>{errors.age}</Tag>}
-                        <Button
-                            type="submit"
-                            disabled={isSubmitting | (touched && !isValid)}
-                            onClick={() => submitForm()}
-                        >
-                            Submit
-                        </Button>
-                    </form>
-                )}
-            </Formik>
-        );
-    }
-}
+            return errors;
+        }}
+        onSubmit={(student, { setSubmitting }) => {
+            addNewStudent(student).then(() => {
+                props.onSuccess();
+                setSubmitting(false);
+            })
+        }}
+    >
+        {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            submitForm,
+            isValid
+        }) => (
+            <form onSubmit={handleSubmit}>
+                <Input
+                    style={inputBottomMargin}
+                    name="firstName"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.firstName}
+                    placeholder='First name'
+                />
+                {errors.firstName && touched.firstName && <Tag style={tagStyle}>{errors.firstName}</Tag>}
+                <Input
+                    style={inputBottomMargin}
+                    name="age"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.age}
+                    placeholder='Age'
+                />
+                {errors.age && touched.age && <Tag style={tagStyle}>{errors.age}</Tag>}
+                <Button
+                    type="submit"
+                    disabled={isSubmitting | (touched && !isValid)}
+                    onClick={() => submitForm()}
+                >
+                    Submit
+                </Button>
+            </form>
+        )}
+    </Formik>
+);
 
 export default AddStudentForm;
